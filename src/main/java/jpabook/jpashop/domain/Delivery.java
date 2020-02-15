d400650ecc2b07c0627533e28113fe39213aa2ba
Geonwoo Jeong <geonwoo.jeong@gmail.com>
2020-02-15 15:43:47 +0900

    Add Entity Class Part 1

---
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -0,0 +1,24 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+public class Delivery {
+
+    @Id @GeneratedValue
+    @Column(name = "delivery_id")
+    private Long id;
+
+    @OneToOne(mappedBy = "delivery")
+    private Order order;
+
+    @Embedded
+    private Address address;
+
+    @Enumerated(EnumType.STRING) // ORDINAL은 새로운 상태가 추가될 경우 값이 망가지므로 사용금지
+    private DeliveryStatus status; // READY, COMP
+}
---
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,26 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String name;
+
+    @Embedded
+    private Address address;
+
+    @OneToMany(mappedBy = "member") // 마스터가 아니게 만들어서 값을 변경해도 foreign key가 변경되지 않도록 함
+    private List<Order> orders = new ArrayList<>();
+
+}
