6b282f7a467e9519f9b6bfe9b7fd02eeceaa3532
Geonwoo Jeong <geonwoo.jeong@gmail.com>
2020-02-16 20:00:12 +0900

    Item Entity

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,7 +12,7 @@ import java.util.List;
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE) // 테이블 구성 전략
 @DiscriminatorColumn(name = "dtype") // 구별자 컬럼
-@Getter @Setter
+@Getter
 public abstract class Item {
 
     @Id
@@ -25,4 +26,21 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    // Business Logic
+
+    /*
+     * stock increase
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock <0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
